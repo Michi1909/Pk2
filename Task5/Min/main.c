834ff98f6628a09b12380eccc89a7fb56e6f481c
Michi <michaela.kaiser1993@outlook.de>
2022-01-25 12:49:24 +0100

    added Task5/min

---
+++ Task5/Min/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "minimal.h"
+
+int main(void)
+{
+    int n = 5;
+    double a[5] = {2, 6, 3, 1, 4};
+    minimal(a, n);
+    return 0;
+}
---
+++ Task5/Min/minimal.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "minimal.h"
+
+double min(double a[], int n)
+{
+    double temp = a[0];
+    for (int i = 0; i < n; i++)
+    {
+        if (temp < a[i])
+        {
+            temp = a[i];
+        }
+    }
+    return temp;
+}
